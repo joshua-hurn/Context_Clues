c31c725ee2331a9addb236f245ffd7088dc37f18
joshua-hurn <joshua.hurn@hotmail.com>
2018-09-25 13:18:25 -0500

    program functional;

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var totalAcc = 101;
-    var friends = ['susie Q', 'james deen', 'mark ruffalo', 'ben stein', 'tom hanks']
-    var locations = ['kitchen', 'stairwell', 'shed', 'bedroom,', 'study', 'bathroom', 'greenhouse', 'garage', 'lab', 'library']
-    var weapons = ['hammer', 'anvil', 'axe', 'sword', 'knife', 'ocelot', 'pen', 'plate', 'mace', 'bow and arrow', 'antiballistic missile', 'boomerang', 'flamethrower', 'spear', 'mortar', 'tank', 'plague', 'lance', 'pike', 'tomahawk']
+for (var i = 0; i <= 100; i++) {
+    var h3 = document.createElement('h3');
+    var textnode = document.createTextNode("Accusation" + " " + i);
+    h3.appendChild(textnode);
+    document.body.appendChild(h3);
+    var changeAlert = makeWords(i);
+    h3.addEventListener('click', changeAlert);
 
+    function makeWords(i) {
+        var friends = ['Susie Q', 'James Deen', 'Mark Ruffalo', 'Ben Stein', 'Tom Hanks'];
+        var weapons = ['hammer', 'anvil', 'axe', 'sword', 'knife', 'ocelot', 'pen', 'plate', 'mace', 'bow and arrow',
+            'antiballistic missile', 'boomerang', 'flamethrower', 'spear', 'mortar', 'tank', 'plague', 'lance', 'pike', 'tomahawk'];
+        var locations = ['kitchen', 'stairwell', 'shed', 'bedroom,', 'study', 'bathroom', 'greenhouse', 'garage', 'lab', 'library'];
 
-
-    for (var i = 0; i < totalAcc; i++) {
-        var h3 = document.createElement('h3');
-        var textnode = document.createTextNode("Accusation" + " " + i);
-        h3.appendChild(textnode);
-        body.appendChild(h3)
-    }
-});
-
-function myFunc () {
-    return h3.addEventListener('click', function () {
-        alert('Accusation' + i + ': ' + 'I accuse ' + friends[i] + ' with the ' + weapons[i] + ' in the ' + locations[i] + '!')
-    }
+        function changeAlert() {
+            alert(`'Accusation ${[i]}: I accuse ${friends[i % friends.length]} with the ${weapons[i % weapons.length]} in the ${locations[i % locations.length]}!'`);
+        }
+        return changeAlert;
+    };
 }
